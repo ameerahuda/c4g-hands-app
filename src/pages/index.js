e5fcaf6017527d34e8d9b946a39446a88cd49178
Ameera Huda <ahuda6@gatech.edu>
2023-02-17 20:26:36 -0600

    Test Vercel deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,7 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.js</code>
           </p>
+          <p>Welcome to HandS Project App</p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
