703755e7c5f4814f5460a9087b5427b121595cc6
Yang Bai <ybai300@gatech.edu>
2023-02-22 20:51:55 +0800

    init db connection and apis for sign-in

---
+++ database/DB_DDL.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE c4g_hands_db IF NOT EXISTS;
+
+use c4g_hands_db;
+
+CREATE TABLE user (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    password VARCHAR(50),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+describe user;
